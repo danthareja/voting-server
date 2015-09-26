26664cab4f6db53690322377e1c1c50f3e3d3e30
Dan Thareja <danthareja@gmail.com>
2015-09-26 15:31:07 -0700

    adds socket.io

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-import makeStore from '.src/store'
+import makeStore from './src/store'
+import startServer from './src/server'
+import initialEntries from './entries.json'
 
-export const store = makeStore();
+export const store = makeStore()
+startServer(store)
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: initialEntries
+})
+store.dispatch({ type: 'NEXT' })
